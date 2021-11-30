6b7e8945d4076f315278c7e1502c47cdf39c0934
Thomas <thomas@craftacademy.se>
2021-11-30 07:14:15 +0100

    small updates

--- spec/BirthNumber.spec.js
+++ spec/BirthNumber.spec.js
@@ -5,21 +5,22 @@ describe("BirthNumber", () => {
   def("data", { day: 1, month: 1, year: 1970 });
   
   it(() => is.expected.to.be.an.instanceOf(BirthNumber));
-  it(() => is.expected.to.respondTo("calculate"));
-
+  
   it("is expected to be an instance of described class", () => {
     expect($subject).to.be.an.instanceOf(BirthNumber);
   });
+
+  it(() => is.expected.to.respondTo("calculate"));
   
-  describe('Thomas 1971-11-24', () => {
-    def("data", { day: 24, month: 11, year: 1971 });
+  describe('for Thomas 1971-11-24', () => {
+    def("data", { day: "24", month: "11", year: "1971" });
     it('is expected to return 8', () => {
       expect($subject.calculate()).to.equal(8)
     });
   });
 
-  describe('Thadeus 2014-1-3', () => {
-    def("data", { day: 3, month: 1, year: 2014 });
+  describe('for Thadeus 2014-1-3', () => {
+    def("data", { day: "3", month: "1", year: "2014" });
     it('is expected to return 11', () => {
       expect($subject.calculate()).to.equal(11)
     });
